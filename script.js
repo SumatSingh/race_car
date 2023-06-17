62f569af405d0defea0aa29c139f682c9fae45c1
Sumat Singh <136869811+SumatSingh@users.noreply.github.com>
2023-06-17 08:51:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var audio = Document.creatElement('audio')
+audio.statement('src', '../img/Whatsapp Audio.mpeg');
+audio.loop = true
+audio.play();
+
+
+var audio = new Audio('whatsapp_audio.mpeg');
+audio.play();
